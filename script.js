c0dbf0c5794c2ccd395de068e828dd52934a6425
Luis-Filipe-Santos <109242677+Luis-Filipe-Santos@users.noreply.github.com>
2022-08-24 17:35:11 -0300

    f

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ let musicas = [
     {titulo:'Vem me buscar', artista:'Jefferson & Suellen', src:'./musicas/VEM ME BUSCAR - JEFFERSON _ SUELLEN (LIVE SESSION - AO VIVO)(MP3_320K).mp3', img:'./imagens/VemBuscar.png'}    
 ];
 
+// Inicio
 let musica = document.querySelector('audio');
 let indexMusica = 0;
 
-let duracaoMusica = document.querySelector('.fim');
-let imagem = document.querySelector('img');
 let nomeMusica = document.querySelector('.descricao h2');
 let nomeArtista = document.querySelector('.descricao i');
+let imagem = document.querySelector('img');
+let tempoDecorrido = document.querySelector('.tempo .inicio');
+let duracaoMusica = document.querySelector('.tempo .fim');
 
 renderizarMusica(indexMusica);
 
@@ -66,21 +68,20 @@ function pararMusica(){
     document.querySelector('.botao-pause').style.display = 'none';
 }
 
-function atualizarBarra(){
-    let barra = document.querySelector('progress');
-    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
-    let tempoDecorrido = document.querySelector('.inicio');
-    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
-}
-
 function segundosParaMinutos(segundos){
     let campoMinutos = Math.floor(segundos / 60);
     let campoSeguntos = segundos % 60;
+
     if(campoSeguntos < 10){
         campoSeguntos = '0' + campoSeguntos;
     }
-
-    return campoMinutos + ':' + campoSeguntos;
+    
+    return `${campoMinutos}:${campoSeguntos}`;
 }
 
+function atualizarBarra(){
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+}
 
